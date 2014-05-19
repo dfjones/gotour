5702eede06f3edcf05c10a42a8f476cd62b4a51f
Doug Jones <doug@dfjones.net>
2014-05-19 19:14:24 -0400

    Remove repeated code.

--- 73/serial.go
+++ 73/serial.go
@@ -15,7 +15,7 @@ type visitMap map[string]bool
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
 
-func Crawl(url string, depth int, fetcher Fetcher) (map[string]bool) {
+func SerialCrawl(url string, depth int, fetcher Fetcher) (map[string]bool) {
   vm := make(visitMap)
   crawl(url, depth, fetcher, vm)
   return vm
@@ -41,54 +41,5 @@ func crawl(url string, depth int, fetcher Fetcher, visited visitMap) {
 }
 
 func main() {
-    Crawl("http://golang.org/", 4, fetcher)
-}
-
-// fakeFetcher is Fetcher that returns canned results.
-type fakeFetcher map[string]*fakeResult
-
-type fakeResult struct {
-    body string
-    urls []string
-}
-
-func (f fakeFetcher) Fetch(url string) (string, []string, error) {
-    if res, ok := f[url]; ok {
-        return res.body, res.urls, nil
-    }
-    return "", nil, fmt.Errorf("not found: %s", url)
-}
-
-// fetcher is a populated fakeFetcher.
-var fetcher = fakeFetcher{
-    "http://golang.org/": &fakeResult{
-        "The Go Programming Language",
-        []string{
-            "http://golang.org/pkg/",
-            "http://golang.org/cmd/",
-        },
-    },
-    "http://golang.org/pkg/": &fakeResult{
-        "Packages",
-        []string{
-            "http://golang.org/",
-            "http://golang.org/cmd/",
-            "http://golang.org/pkg/fmt/",
-            "http://golang.org/pkg/os/",
-        },
-    },
-    "http://golang.org/pkg/fmt/": &fakeResult{
-        "Package fmt",
-        []string{
-            "http://golang.org/",
-            "http://golang.org/pkg/",
-        },
-    },
-    "http://golang.org/pkg/os/": &fakeResult{
-        "Package os",
-        []string{
-            "http://golang.org/",
-            "http://golang.org/pkg/",
-        },
-    },
+    SerialCrawl("http://golang.org/", 4, fetcher)
 }
