5602375e7a291b90659575a003e39268bf306415
Doug Jones <doug@dfjones.net>
2014-05-19 14:36:05 -0400

    Move common boilerplate to its own file.

--- 73/crawl.go
+++ 73/crawl.go
@@ -4,20 +4,6 @@ import (
     "fmt"
 )
 
-
-/*
-insructions:
-In this exercise you'll use Go's concurrency features to parallelize a web crawler.
-
-Modify the Crawl function to fetch URLs in parallel without fetching the same URL twice.
-*/
-
-type Fetcher interface {
-    // Fetch returns the body of URL and
-    // a slice of URLs found on that page.
-    Fetch(url string) (body string, urls []string, err error)
-}
-
 type VisitRegister interface {
   Visit(string)
   IsVisited(string) bool
@@ -115,54 +101,5 @@ func crawl(done chan<- struct{}, url string, depth int, visitRegister VisitRegis
 }
 
 func main() {
-    Crawl("http://golang.org/", 4, fetcher)
-}
-
-// fakeFetcher is Fetcher that returns canned results.
-type fakeFetcher map[string]*fakeResult
-
-type fakeResult struct {
-    body string
-    urls []string
-}
-
-func (f fakeFetcher) Fetch(url string) (string, []string, error) {
-    if res, ok := f[url]; ok {
-        return res.body, res.urls, nil
-    }
-    return "", nil, fmt.Errorf("not found: %s", url)
-}
-
-// fetcher is a populated fakeFetcher.
-var fetcher = fakeFetcher{
-    "http://golang.org/": &fakeResult{
-        "The Go Programming Language",
-        []string{
-            "http://golang.org/pkg/",
-            "http://golang.org/cmd/",
-        },
-    },
-    "http://golang.org/pkg/": &fakeResult{
-        "Packages",
-        []string{
-            "http://golang.org/",
-            "http://golang.org/cmd/",
-            "http://golang.org/pkg/fmt/",
-            "http://golang.org/pkg/os/",
-        },
-    },
-    "http://golang.org/pkg/fmt/": &fakeResult{
-        "Package fmt",
-        []string{
-            "http://golang.org/",
-            "http://golang.org/pkg/",
-        },
-    },
-    "http://golang.org/pkg/os/": &fakeResult{
-        "Package os",
-        []string{
-            "http://golang.org/",
-            "http://golang.org/pkg/",
-        },
-    },
+    Crawl("http://golang.org/", 4, FakeFetcher)
 }
