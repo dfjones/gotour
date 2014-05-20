a7b0b5738079068d6c709339ea75d2349d62738c
Doug Jones <doug@dfjones.net>
2014-05-19 23:19:40 -0400

    Add a benchmark.

--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -1,15 +1,25 @@
-package 73
+package main
 
-var graph
+import (
+  "fmt"
+  "runtime"
+  "testing"
+)
+
+var depth = 50
+var graphSize = 100000
+var graph Fetcher
 
 func init() {
   numCpus := runtime.NumCPU()
   runtime.GOMAXPROCS(runtime.NumCPU())
   fmt.Println("Running using num cpus: ", numCpus)
-  graph = NewFakeFetcher(100000, 1, 10)
-  fmt.Println("Graph built size: ", len(graph))
+  graph = NewFakeFetcher(graphSize, 1, 10)
+  fmt.Println("Graph built size: ", graphSize)
 }
 
 func BenchmarkChanCrawl(b *testing.B) {
-  visited := ChanCrawl("0", 4, graph)
+  visited := ChanCrawl("0", depth, graph)
+  b.StopTimer()
+  b.Log("Benchmark finished. Visited: ", len(visited))
 }
