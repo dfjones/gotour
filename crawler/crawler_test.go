cd4557426eedf56e2e1dc827da9527e583d1d5e1
Doug Jones <doug@dfjones.net>
2014-05-20 12:17:55 -0400

    Fix impl of serial crawl. Add a test.

--- crawler/crawler_test.go
+++ crawler/crawler_test.go
@@ -1,37 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"runtime"
-	"testing"
+  "fmt"
+  "reflect"
+  "runtime"
+  "testing"
 )
 
-var depth = 10
+var depth = 8
 var graphSize = 100000
 var graph Fetcher
+var smallGraph Fetcher
 
 func init() {
-	numCpus := runtime.NumCPU()
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	fmt.Println("Running using num cpus: ", numCpus)
-	graph = NewFakeFetcher(graphSize, 1, 10)
-	fmt.Println("Graph built size: ", graphSize)
+  numCpus := runtime.NumCPU()
+  runtime.GOMAXPROCS(runtime.NumCPU())
+  fmt.Println("Running using num cpus: ", numCpus)
+  smallGraph = NewFakeFetcher(100, 1, 5)
+  graph = NewFakeFetcher(graphSize, 1, 10)
+  fmt.Println("Graph built size: ", graphSize)
+}
+
+func TestCrawlEquiv(t *testing.T) {
+  depth := 3
+  v1 := SerialCrawl("0", depth, smallGraph)
+  v2 := ChanCrawl("0", depth, smallGraph)
+  v3 := MutexCrawl("0", depth, smallGraph)
+
+  if !reflect.DeepEqual(v1, v2) {
+    t.Errorf("%v != %v", v1, v2)
+  }
+
+  if !reflect.DeepEqual(v1, v3) {
+    t.Errorf("%v != %v", v1, v3)
+  }
 }
 
 func BenchmarkSerialCrawl(b *testing.B) {
-	visited := SerialCrawl("0", depth, graph)
-	b.StopTimer()
-	b.Log("Benchmark finished. Visited: ", len(visited))
+  visited := SerialCrawl("0", depth, graph)
+  b.StopTimer()
+  b.Log("Benchmark finished. Visited: ", len(visited))
 }
 
 func BenchmarkRecursiveChanCrawl(b *testing.B) {
-	visited := ChanCrawl("0", depth, graph)
-	b.StopTimer()
-	b.Log("Benchmark finished. Visited: ", len(visited))
+  visited := ChanCrawl("0", depth, graph)
+  b.StopTimer()
+  b.Log("Benchmark finished. Visited: ", len(visited))
 }
 
 func BenchmarkRecursiveMutexCrawl(b *testing.B) {
-	visited := MutexCrawl("0", depth, graph)
-	b.StopTimer()
-	b.Log("Benchmark finished. Visited: ", len(visited))
+  visited := MutexCrawl("0", depth, graph)
+  b.StopTimer()
+  b.Log("Benchmark finished. Visited: ", len(visited))
 }
--- crawler/serial.go
+++ crawler/serial.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "fmt"
+  "fmt"
 )
 
 type visitMap map[string]bool
@@ -9,27 +9,29 @@ type visitMap map[string]bool
 // Crawl uses fetcher to recursively crawl
 // pages starting with url, to a maximum of depth.
 
-func SerialCrawl(url string, depth int, fetcher Fetcher) (map[string]bool) {
+// SerialCrawl performs the crawl operation with no concurrency
+func SerialCrawl(url string, depth int, fetcher Fetcher) map[string]bool {
   vm := make(visitMap)
   serialcrawl(url, depth, fetcher, vm)
   return vm
 }
 
 func serialcrawl(url string, depth int, fetcher Fetcher, visited visitMap) {
-    if depth <= 0 {
-        return
-    }
-    if _, ok := visited[url]; ok {
-      return
-    }
-    _, urls, err := fetcher.Fetch(url)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    //fmt.Printf("found: %s %q\n", url, body)
-    for _, u := range urls {
-        serialcrawl(u, depth-1, fetcher, visited)
-    }
+  if depth <= 0 {
     return
+  }
+  if _, ok := visited[url]; ok {
+    return
+  }
+  visited[url] = true
+  _, urls, err := fetcher.Fetch(url)
+  if err != nil {
+    fmt.Println(err)
+    return
+  }
+  //fmt.Printf("found: %s %q\n", url, body)
+  for _, u := range urls {
+    serialcrawl(u, depth-1, fetcher, visited)
+  }
+  return
 }
