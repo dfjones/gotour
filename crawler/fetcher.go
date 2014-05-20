8177cf43c7217152ccb6a7a6053f42e3ff248538
Doug Jones <doug@dfjones.net>
2014-05-20 16:48:54 -0400

    Formatting.

--- crawler/fetcher.go
+++ crawler/fetcher.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+  "fmt"
+  "math/rand"
+  "time"
 )
 
 type Fetcher interface {
-    // Fetch returns the body of URL and
-    // a slice of URLs found on that page.
-    Fetch(url string) (body string, urls []string, err error)
+  // Fetch returns the body of URL and
+  // a slice of URLs found on that page.
+  Fetch(url string) (body string, urls []string, err error)
 }
 
 // fakeFetcher is Fetcher that returns canned results.
 type fakeFetcher map[string]*fakeResult
 
 type fakeResult struct {
-    body string
-    urls []string
+  body string
+  urls []string
 }
 
 func (f fakeFetcher) Fetch(url string) (string, []string, error) {
-    time.Sleep(100 * time.Millisecond)
-    if res, ok := f[url]; ok {
-        return res.body, res.urls, nil
-    }
-    return "", nil, fmt.Errorf("not found: %s", url)
+  time.Sleep(100 * time.Millisecond)
+  if res, ok := f[url]; ok {
+    return res.body, res.urls, nil
+  }
+  return "", nil, fmt.Errorf("not found: %s", url)
 }
 
-func NewFakeFetcher(results, minLinks, maxLinks int) (Fetcher) {
+func NewFakeFetcher(results, minLinks, maxLinks int) Fetcher {
   ff := make(fakeFetcher)
   for i := 0; i < results; i++ {
     body := fmt.Sprint(i)
@@ -58,34 +58,34 @@ func NewFakeFetcher(results, minLinks, maxLinks int) (Fetcher) {
 
 // fetcher is a populated fakeFetcher.
 var FakeFetcher = fakeFetcher{
-    "http://golang.org/": &fakeResult{
-        "The Go Programming Language",
-        []string{
-            "http://golang.org/pkg/",
-            "http://golang.org/cmd/",
-        },
+  "http://golang.org/": &fakeResult{
+    "The Go Programming Language",
+    []string{
+      "http://golang.org/pkg/",
+      "http://golang.org/cmd/",
     },
-    "http://golang.org/pkg/": &fakeResult{
-        "Packages",
-        []string{
-            "http://golang.org/",
-            "http://golang.org/cmd/",
-            "http://golang.org/pkg/fmt/",
-            "http://golang.org/pkg/os/",
-        },
+  },
+  "http://golang.org/pkg/": &fakeResult{
+    "Packages",
+    []string{
+      "http://golang.org/",
+      "http://golang.org/cmd/",
+      "http://golang.org/pkg/fmt/",
+      "http://golang.org/pkg/os/",
     },
-    "http://golang.org/pkg/fmt/": &fakeResult{
-        "Package fmt",
-        []string{
-            "http://golang.org/",
-            "http://golang.org/pkg/",
-        },
+  },
+  "http://golang.org/pkg/fmt/": &fakeResult{
+    "Package fmt",
+    []string{
+      "http://golang.org/",
+      "http://golang.org/pkg/",
     },
-    "http://golang.org/pkg/os/": &fakeResult{
-        "Package os",
-        []string{
-            "http://golang.org/",
-            "http://golang.org/pkg/",
-        },
+  },
+  "http://golang.org/pkg/os/": &fakeResult{
+    "Package os",
+    []string{
+      "http://golang.org/",
+      "http://golang.org/pkg/",
     },
+  },
 }
